65b0f593c53b18bcdc1df33eab9f3b7d8a5b2cc7
FileOtter <kevin.t.connell@gmail.com>
2020-09-03 15:15:48 -0400

    added error message for when user does not specify any character set

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//user input 
 var passLength = 0;
 var passPossible = [];
 
@@ -17,9 +17,9 @@ var lowerSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 var upperSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var numberSet = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var numberSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-var specialSet = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-",".", "/", ":", "<", "=", ">", "?", "@", "[", "]", "_", "{", "}"];
+var specialSet = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", "<", "=", ">", "?", "@", "[", "]", "_", "{", "}"];
 
 var isLower = false;
 var isUpper = false;
@@ -34,31 +34,40 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   passPossible = [];
   userPassword = "";
+  var isLower = false;
+  var isUpper = false;
+  var isNumber = false;
+  var isSpecial = false;
 
   var passLength = prompt("How long would you like your password to be?");
   while (passLength < 8 || passLength > 128) {
     alert("The password must be at least 8 characters and no more than 128 characters. " + passLength + " does not meet the critiria");
     var passLength = prompt("How long would you like your password to be?");
   }
-  isLower = confirm("Would you like to include lowercase letters?")
-  if (isLower) {
-    passPossible = passPossible.concat(lowerSet)
-    console.log(passPossible)
-  }
-  isUpper = confirm("Would you like to include uppercase letters?")
-  if (isUpper) {
-    passPossible = passPossible.concat(upperSet)
-    console.log(passPossible)
-  }
-  isNumber = confirm("Would you like to include numbers?")
-  if (isNumber) {
-    passPossible = passPossible.concat(numberSet)
-    console.log(passPossible)
-  }
-  isSpecial = confirm("Would you like to include special characters?")
-  if (isSpecial) {
-    passPossible = passPossible.concat(specialSet)
-    console.log(passPossible)
+  while (isLower == false && isUpper == false && isNumber == false && isSpecial == false) {
+    isLower = confirm("Would you like to include lowercase letters?")
+    if (isLower) {
+      passPossible = passPossible.concat(lowerSet)
+      console.log(passPossible)
+    }
+    isUpper = confirm("Would you like to include uppercase letters?")
+    if (isUpper) {
+      passPossible = passPossible.concat(upperSet)
+      console.log(passPossible)
+    }
+    isNumber = confirm("Would you like to include numbers?")
+    if (isNumber) {
+      passPossible = passPossible.concat(numberSet)
+      console.log(passPossible)
+    }
+    isSpecial = confirm("Would you like to include special characters?")
+    if (isSpecial) {
+      passPossible = passPossible.concat(specialSet)
+      console.log(passPossible)
+    }
+    if (isLower == false && isUpper == false && isNumber == false && isSpecial == false) {
+      alert("You must selective at least one character type!")
+    }
   }
   for (var i = 0; i < passLength; i++) {
     randomNumber = Math.floor(Math.random() * passPossible.length);
